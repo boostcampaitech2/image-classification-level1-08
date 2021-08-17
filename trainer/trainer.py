07efb19e8b3cf22b4bcdde6076ed5eb1a3a4668d
lsh <easter3163@naver.com>
2021-08-18 03:05:45 +0900

    cifar1-

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -41,9 +41,10 @@ class Trainer(BaseTrainer):
         self.train_metrics.reset()
         for batch_idx, (data, target) in enumerate(self.data_loader):
             data, target = data.to(self.device), target.to(self.device)
-
+            
             self.optimizer.zero_grad()
             output = self.model(data)
+
             loss = self.criterion(output, target)
             loss.backward()
             self.optimizer.step()
