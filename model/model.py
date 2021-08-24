84d0f486cbba4256046aa4aca2594f591dfd12a0
lsh <easter3163@naver.com>
2021-08-24 17:39:53 +0900

    Clipmodel

--- model/model.py
+++ model/model.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 from base import BaseModel
 import torch
 import torchvision
-
+import clip
 
 class MnistModel(BaseModel):
     def __init__(self, num_classes=10):
@@ -52,21 +52,51 @@ class ResNet18(BaseModel):
         x = self.feature_extractor(x)
         return F.log_softmax(x, dim=1)
 
-class ClipModel(BaseModel):
-    def __init__(self):
+class ResNet34(BaseModel):
+    def __init__(self, num_classes=18):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.feature_extractor = torchvision.models.resnet34(pretrained=True)
+        self.feature_extractor.fc = nn.Linear(512, num_classes)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.feature_extractor(x)
         return F.log_softmax(x, dim=1)
+
+class ClipModel(BaseModel):
+    def __init__(self, num_classes=18):
+        super().__init__()
+        self.model, self.preprocess = clip.load("ViT-B/32")
+        self.fc1 = nn.Linear(512, 256)
+        self.batch = nn.BatchNorm1d(256)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(256, num_classes)
+
+    def forward(self, x):
+        with torch.no_grad():
+            x = self.model.encode_image(x)
+        x = x.float()
+
+        x = self.fc1(x)
+        x = self.batch(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=-1)
+
+
+class ClipThreeHeadModel(BaseModel):
+    def __init__(self, num_classes=18):
+        super().__init__()
+        self.model, self.preprocess = clip.load("ViT-B/32")
+
+        self.fc = nn.Linear(512, 256)
+        self.relu = nn.ReLU()
+        self.fc1 = nn.Linear(256, 3)
+        self.fc2 = nn.Linear(256, 2)
+        self.fc3 = nn.Linear(256, 3)
+
+    def forward(self, x):
+        with torch.no_grad():
+            x = self.model.encode_image(x)
+        x = x.float()
+        x = self.relu(self.fc(x))
+        return F.log_softmax(self.fc1(x), dim=-1), F.log_softmax(self.fc2(x), dim=-1), F.log_softmax(self.fc3(x), dim=-1)
