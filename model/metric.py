127002262e0b9388f4cc2ba596f2dfc4ff823e67
MyungHoon Jin <jinmang2@gmail.com>
2021-08-24 11:24:04 +0900

    Add recall, precision, and f1

--- model/metric.py
+++ model/metric.py
@@ -1,4 +1,5 @@
 import torch
+from sklearn.metrics import precision_recall_fscore_support
 
 
 def accuracy(output, target):
@@ -18,3 +19,24 @@ def top_k_acc(output, target, k=3):
         for i in range(k):
             correct += torch.sum(pred[:, i] == target).item()
     return correct / len(target)
+
+
+def precision(output, target):
+    precision, _, _, _ = precision_recall_fscore_support(
+        target, output, average='macro',
+    )
+    return precision
+
+
+def recall(output, target):
+    _, recall, _, _ = precision_recall_fscore_support(
+        target, output, average='macro',
+    )
+    return recall
+
+
+def f1(output, target):
+    _, _, f1, _ = precision_recall_fscore_support(
+        target, output, average='macro',
+    )
+    return f1
