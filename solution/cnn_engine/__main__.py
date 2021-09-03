0698e7eb47198ab58c9cfb5b783cb7fb64c4b178
lsh <easter3163@naver.com>
2021-09-03 22:14:38 +0900

    Main

--- solution/cnn_engine/__main__.py
+++ solution/cnn_engine/__main__.py
@@ -1,6 +1,9 @@
+import subprocess
+
+
 def main():
-    print("Test main")
-
-
+    subprocess.call(['python3', 'train.py', '-c', "config.json"])
+    subprocess.call(['python3', 'infer.py'])
+    
 if __name__ == '__main__':
     main()
