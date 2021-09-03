0879b50ed2f9acbc840b89e227076da3f6cb57ae
MyungHoon Jin <jinmang2@gmail.com>
2021-09-03 20:19:41 +0900

    Delete commit

--- model/loss.py
+++
@@ -1,9 +0,0 @@
-import torch.nn.functional as F
-
-
-def nll_loss(output, target):
-    return F.nll_loss(output, target)
-
-
-def cross_entropy_loss(output, target):
-    return F.cross_entropy_loss(output, target)
--- trainer/__init__.py
+++
@@ -1,6 +0,0 @@
-from .trainer import *
-
-try:
-    from transformers import Trainer as HfTrainer
-except ImportError:
-    pass
